64979b8907eb1a6262b3a55856123840070061e2
MaxHwoy <max.hwoy@gmail.com>
2020-04-13 13:48:47 -0700

    Added FieldInfoX extensions.

--- CoreExtensions.Reflection/FieldInfoX.cs
+++ CoreExtensions.Reflection/FieldInfoX.cs
@@ -13,5 +13,29 @@ namespace CoreExtensions.Reflection
 			return (!typeof(string).Equals(field.FieldType) &&
 				typeof(IEnumerable).IsAssignableFrom(field.FieldType));
 		}
+
+		public static FieldInfo GetFastProperty(this object obj, string name)
+			=> obj.GetType().GetField(name ?? string.Empty);
+
+		public static MethodInfo GetFastMethod(this FieldInfo field, string method, Type[] args)
+			=> field.FieldType.GetMethod(method ?? string.Empty, args);
+
+		public static MethodInfo GetFastMethod(this object obj, string field, string method, Type[] args)
+			=> obj.GetType().GetField(field ?? string.Empty)?.FieldType
+				.GetMethod(method ?? string.Empty, args);
+
+		public static object FastMethodInvoke(this FieldInfo field, object obj, string method,
+			Type[] args, object[] attr)
+			=> field.FieldType.GetMethod(method ?? string.Empty, args)?.Invoke(obj, attr);
+
+		public static object FastMethodInvoke(this object obj, string field, string method,
+			Type[] args, object[] attr)
+		{
+			var info = obj.GetType().GetField(field ?? string.Empty);
+			if (info == null) return null;
+			return info.FieldType
+				.GetMethod(method ?? string.Empty)
+				?.Invoke(info.GetValue(obj), attr);
+		}
 	}
 }
