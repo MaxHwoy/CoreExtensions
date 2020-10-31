05d56c51b3002d34da485f39cad926ca41bc9bad
MaxHwoy <max.hwoy@gmail.com>
2020-10-31 15:27:13 -0700

    Added ISquareMatrix interface

---
+++ CoreExtensions/Numeric/ISquareMatrix.cs
@@ -0,0 +1,19 @@
+﻿namespace CoreExtensions.Numeric
+{
+	public interface ISquareMatrix : IMatrix
+	{
+		float Determinant { get; }
+		ISquareMatrix IdentityMatrix { get; }
+		bool IsDiagonal { get; }
+		bool IsIdempotent { get; }
+		bool IsIdentity { get; }
+		bool IsInvertible { get; }
+		bool IsLowerTriangular { get; }
+		bool IsSkewSymmetric { get; }
+		bool IsSymmetric { get; }
+		bool IsUpperTriangular { get; }
+		float Trace { get; }
+
+		ISquareMatrix Invert();
+	}
+}
