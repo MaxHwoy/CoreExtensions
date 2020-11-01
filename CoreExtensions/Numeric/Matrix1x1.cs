861a87c327d092d8d418249c1cad92f49172187e
MaxHwoy <max.hwoy@gmail.com>
2020-10-31 18:11:09 -0700

    Added Matrix1x1 struct

---
+++ CoreExtensions/Numeric/Matrix1x1.cs
@@ -0,0 +1,90 @@
+﻿using System;
+using System.Runtime.InteropServices;
+
+
+
+namespace CoreExtensions.Numeric
+{
+	[StructLayout(LayoutKind.Sequential, Pack = 1)]
+	public struct Matrix1x1 : ISquareMatrix, IMatrix
+	{
+		public float Value11 { get; set; }
+
+		public static Matrix1x1 Zero => new Matrix1x1(0);
+		public static Matrix1x1 Identity => new Matrix1x1(1);
+		public int Columns => 1;
+		public float Determinant => this.Value11;
+		public ISquareMatrix IdentityMatrix => Identity;
+		public bool IsDiagonal => true;
+		public bool IsIdempotent => this.Value11 == 1f || this.Value11 == 0f;
+		public bool IsIdentity => this == Identity;
+		public bool IsInvertible => this.Determinant != 0;
+		public bool IsLowerTriangular => true;
+		public bool IsSkewSymmetric => this.Value11 == 0f;
+		public bool IsSymmetric => true;
+		public bool IsUpperTriangular => true;
+		public int Rows => 1;
+		public float Trace => this.Value11;
+		public IMatrix ZeroMatrix => Zero;
+
+		public Matrix1x1(Matrix1x1 matrix) => this.Value11 = matrix.Value11;
+		public Matrix1x1(float a11) => this.Value11 = a11;
+
+		public float this[int index]
+		{
+			get
+			{
+				if (index == 0) return this.Value11;
+				else throw new ArgumentOutOfRangeException("Index can only be 0");
+			}
+			set
+			{
+				if (index == 0) this.Value11 = value;
+				else throw new ArgumentOutOfRangeException("Index can only be 0");
+			}
+		}
+		public float this[int row, int column]
+		{
+			get
+			{
+				if (row == 0 && column == 0) return this.Value11;
+				else throw new ArgumentOutOfRangeException("Row and column indexes should be 1");
+			}
+			set
+			{
+				if (row == 0 && column == 0) this.Value11 = value;
+				else throw new ArgumentOutOfRangeException("Row and column indexes should be 1");
+			}
+		}
+
+		public Matrix1x1 Clone() => new Matrix1x1(this.Value11);
+		public override bool Equals(object obj) => obj is Matrix1x1 matrix && matrix == this;
+		public bool Equals(Matrix1x1 matrix) => this == matrix;
+		public override int GetHashCode() => this.Value11.GetHashCode();
+		public Matrix1x1 Invert()
+		{
+			if (!this.IsInvertible) return Zero;
+			else return new Matrix1x1(1 / this.Value11);
+		}
+		ISquareMatrix ISquareMatrix.Invert() => this.Invert();
+		public Matrix1x1 Power(int power) => this ^ power;
+
+		public Matrix1x1 Transpose() => new Matrix1x1(this.Value11);
+		IMatrix IMatrix.Transpose() => this.Transpose();
+		public override string ToString() => this.Value11.ToString();
+
+		public static bool operator ==(Matrix1x1 a, Matrix1x1 b) => a.Value11 == b.Value11;
+		public static bool operator !=(Matrix1x1 a, Matrix1x1 b) => a.Value11 != b.Value11;
+		public static Matrix1x1 operator +(Matrix1x1 a, Matrix1x1 b) => new Matrix1x1(a.Value11 + b.Value11);
+		public static Matrix1x1 operator -(Matrix1x1 a, Matrix1x1 b) => new Matrix1x1(a.Value11 - b.Value11);
+		public static Matrix1x1 operator *(Matrix1x1 m, float scalar) => new Matrix1x1(m.Value11 * scalar);
+		public static Matrix1x1 operator *(Matrix1x1 m, int scalar) => new Matrix1x1(m.Value11 * scalar);
+		public static Matrix1x1 operator *(Matrix1x1 a, Matrix1x1 b) => new Matrix1x1(a.Value11 * b.Value11);
+		public static Matrix1x1 operator ^(Matrix1x1 m, int power)
+		{
+			if (power < 0) throw new ArgumentOutOfRangeException("Raised power should be greater than 0");
+			if (power == 0) return Identity;
+			return new Matrix1x1((float)Math.Pow(m.Value11, power));
+		}
+	}
+}
